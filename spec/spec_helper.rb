fa16c7e9e59ff1428551a85a897ecd4f3497803d
karthik <karthik@altiscale.com>
2018-02-10 20:34:53 +0530

    style fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ module IPRoute
         'gre' => '14: tungre0@NONE: <POINTOPOINT,NOARP> mtu 1476 qdisc noqueue state DOWN mode DEFAULT qlen 1\    link/gre 10.1.1.2 peer 10.1.1.1 promiscuity 0 \    gre remote 10.1.1.1 local 10.1.1.2 ttl inherit addrgenmode eui64',
         'alias' => "126: eth0@if127: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP mode DEFAULT\n link/ether 02:42:ac:12:00:02 brd ff:ff:ff:ff:ff:ff link-netnsid 0 promiscuity 0\n veth addrgenmode eui64\n alias i am what i am",
         'veth' => '11: veth1@if10: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN mode DEFAULT qlen 1000\    link/ether 86:1a:45:4d:23:26 brd ff:ff:ff:ff:ff:ff link-netnsid 1 promiscuity 0 \    veth addrgenmode eui64',
-      }
+      },
     }
   end
 end
