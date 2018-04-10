7e5cc8ba26cc9ce0a6ad993cb59e95bb8b29fe05
karthik <karthik@altiscale.com>
2018-04-10 08:56:49 +0530

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'karthik'
 maintainer_email  'kumarkarthikn@gmail.com'
 description       'Provides custom resources for iproute2 commands'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.0.0'
+version           '2.0.1'
 license           'Apache-2.0'
 
 chef_version '>= 12.5'
