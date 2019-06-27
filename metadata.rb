042d2c05af793c6616b2070ec877873b54835221
michael-altiscale <michael@altiscale.com>
2019-06-27 14:37:48 -0500

    Updating the metadata.rb file to use depends.json

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,11 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 license           'Apache-2.0'
 
-chef_version '>= 12.5'
-
+JSON.parse(File.read(File.join(File.dirname(__FILE__), 'depends.json'))).each_pair do |cbk,ver|
+  depends cbk,ver
+end
 source_url "https://github.com/VertiCloud/#{name}-cookbook"
 issues_url "#{source_url}/issues"
+chef_version '>= 12.5'
 
 supports 'prometheus'
